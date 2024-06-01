886e1b57ff0dc6c2f03b18fd7129bb3c28d4ae24
calvinwijaya <calvin.wijaya@mail.ugm.ac.id>
2024-06-01 17:37:50 +0700

    update test.py and upload trained model

--- test.py
+++ test.py
@@ -8,8 +8,9 @@ import torch.nn as nn
 from utils.data import data_loaders
 from utils.model import RandLANet
 
-# Function to read and save las data
 import laspy
+import argparse
+import os
 
 def save_las(X, filename):
     header = laspy.LasHeader(point_format=2, version="1.2")
@@ -23,62 +24,82 @@ def save_las(X, filename):
     las.classification = X[:, 6]
     las.write(filename + ".las")
 
-t0 = time.time()
+def read_las(args):
+    lasfile = laspy.read(args.data)
+    x = lasfile.x
+    y = lasfile.y
+    z = lasfile.z
+    r = lasfile.red
+    g = lasfile.green
+    b = lasfile.blue
+    C = lasfile.classification
+    data = np.column_stack((x, y, z, r, g, b))
+    labels = np.column_stack((C))
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    xy_min = np.amin(data, axis=0)[0:2]
+    data[:, 0:2] -= xy_min
+    
+    return data, labels, xy_min
 
-print('Loading data...')
-las_file = 'data/02_Test Kecil Full Data.las'
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Process LAS files and save as numpy arrays.")
+    parser.add_argument("--data", required=True, help="Path to the LAS file.")
+    parser.add_argument("--log_dir", required=True, help="Model folder.")
+    parser.add_argument("--num_class", required=False, default=2, help="Number of classes.")
+    return parser.parse_args()
 
-lasfile = laspy.read(las_file)
-x = lasfile.x
-y = lasfile.y
-z = lasfile.z
-r = lasfile.red
-g = lasfile.green
-b = lasfile.blue
-C = lasfile.classification
-data = np.column_stack((x, y, z, r, g, b))
-labels = np.column_stack((C))
+def predict(data, labels, args):
+    points = torch.from_numpy(data)
+    labels = torch.from_numpy(labels)
+    
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    
+    print('Loading model...')
 
-xy_min = np.amin(data, axis=0)[0:2]
-data[:, 0:2] -= xy_min
+    d_in = 6
+    num_classes = args.num_class
+    
+    model = RandLANet(d_in, num_classes, 16, 4, device)
+    model.load_state_dict(torch.load('runs/'+args.log_dir+'/checkpoint_100.pth')['model_state_dict'])
+    model.eval()
 
-points = torch.from_numpy(data)
-labels = torch.from_numpy(labels)
+    print('Predicting labels...')
+    with torch.no_grad():
+        points = points.to(device)
+        points = points.float()
+        points = points.unsqueeze(-1)
+        points = points.permute(2, 0, 1)
+        labels = labels.to(device)
+        scores = model(points)
+        predictions = torch.max(scores, dim=-2).indices
+        accuracy = (predictions == labels).float().mean() # TODO: compute mIoU usw.
+        print('Accuracy:', accuracy.item())
+        predictions = predictions.cpu().numpy()
+        
+    return predictions
+    
+if __name__ == "__main__":
+    t0 = time.time()
+    args = parse_arguments()
+    print('Loading data...')
+    data, labels, xy_min = read_las(args)
+    predictions = predict(data, labels, args)
 
-print('Loading model...')
-
-d_in = 6
-num_classes = 2
-
-model = RandLANet(d_in, num_classes, 16, 4, device)
-model.load_state_dict(torch.load('runs/test/checkpoint_10.pth')['model_state_dict'])
-model.eval()
-
-
-print('Predicting labels...')
-with torch.no_grad():
-    points = points.to(device)
-    points = points.float()
-    points = points.unsqueeze(-1)
-    points = points.permute(2, 0, 1)
-    labels = labels.to(device)
-    scores = model(points)
-    predictions = torch.max(scores, dim=-2).indices
-    accuracy = (predictions == labels).float().mean() # TODO: compute mIoU usw.
-    print('Accuracy:', accuracy.item())
-    predictions = predictions.cpu().numpy()
-
-print('Writing results...')
-
-t1 = time.time()
-# write point cloud with classes
-print('Assigning labels to the point cloud...')
-num_rows = data.shape[0]
-predictions = predictions.reshape(num_rows, 1)
-data[:, 0:2] += xy_min
-result = np.hstack((data, predictions))
-save_las(result, 'hasil')
+    t1 = time.time()
+    # write point cloud with classes
+    print('Assigning labels to the point cloud...')
+    num_rows = data.shape[0]
+    predictions = predictions.reshape(num_rows, 1)
+    data[:, 0:2] += xy_min
+    result = np.hstack((data, predictions))
+    
+    result_dir = 'result'
+    if not os.path.exists(result_dir):
+        os.mkdir(result_dir)
+    
+    filename = os.path.basename(str(args.data))
+    filename = os.path.splitext(filename)[0]
+    filename_pred = os.path.join(result_dir, filename + '_pred')
+    save_las(result, filename_pred)
 
 print('Done. Time elapsed: {:.1f}s'.format(t1-t0))
